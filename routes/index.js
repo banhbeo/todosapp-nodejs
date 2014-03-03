8cace60b3a6678da30964aff0d82ad3f58e1087d
BanhBeo <fake@gmail.com>
2014-03-03 13:12:34 +0700

    Add header

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,21 @@ module.exports = function (app) {
     var todo = new app.Todo(req.body);
     todo.save(function (err) {
       if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(todo));
     });
   });
 
   app.get('/todos', function (req, res) {
     app.Todo.find(function (err, todos) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(todos));
     })
   });
 
   app.get('/todos/:id', function (req, res) {
     app.Todo.findById(req.params.id, function (err, todo) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(todo));
     })
   });
@@ -26,6 +29,7 @@ module.exports = function (app) {
   app.delete('/todos/:id', function (req, res) {
     app.Todo.findById(req.params.id, function (err, todo) {
       todo.remove(function (err) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Deleted');
       });
     })
@@ -36,6 +40,7 @@ module.exports = function (app) {
     delete todo._id;
 
     app.Todo.update({ _id: req.params.id }, todo, function (err) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Updated');
     });
   });
